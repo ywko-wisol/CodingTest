d482da1b7921f9301ce8c85ef97f191ba1bf00ba
YangwooKo <ywko@wisol.co.kr>
2014-07-01 16:18:40 +0900

    Simplistic Cookie Example

---
+++ Cookie/Scripts/Main.js
@@ -0,0 +1,11 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// document.cookie="username=Yangwoo Ko";
+// alert(location.search);
+
+document.cookie = "username="+getParameterByName('user');
